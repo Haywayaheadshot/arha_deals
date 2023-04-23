6d08f96a7fce4afd76622d5ea6d4927c2cf8f7cf
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-04-23 08:50:43 +0000

    Create technews component

---
+++ src/components/TechNews/TechNews.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TechNews() {
+  return (
+    <div className="text-black pt-10">
+      <h1 className="mt-2 text-center">TechNews</h1>
+    </div>
+  );
+}
+
+export default TechNews;
