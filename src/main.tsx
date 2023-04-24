24ce980b1f9e39730e9395eda27b1a4a9a44ca6f
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-04-24 02:59:10 +0000

    Succesfully receive and map news to UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
 import "./styles/index.css";
 import "tailwindcss/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
