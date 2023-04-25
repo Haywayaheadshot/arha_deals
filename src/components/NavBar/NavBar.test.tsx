62386641e0dc07426314dae0b09e97bf3781632b
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-04-25 01:08:48 +0000

    Refactor test to include browser routes

--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import NavBar from "./NavBar";
+import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 
-test("renders the title", () => {
-  render(<NavBar />);
-  const titleElement = screen.getByText(/Budget/);
-  expect(titleElement).toBeInTheDocument();
-});
+describe("Check that NavBar component", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    );
+  });
 
-test("displays the current time", () => {
-  render(<NavBar />);
-  const timeElement = screen.getByText(/^\d{1,2}:\d{2}:\d{2}$/); // Matches the format of the displayed time
-  expect(timeElement).toBeInTheDocument();
-});
+  test("renders the title", () => {
+    const titleElement = screen.getByText(/Budget/);
+    expect(titleElement).toBeInTheDocument();
+  });
 
-test("shows the menu when the hamburger is clicked", () => {
-  render(<NavBar />);
-  const hamburgerElement = screen.getByLabelText(/Show menu/);
-  expect(screen.queryByText(/New Stock/)).not.toBeInTheDocument(); // Menu is initially closed
-  fireEvent.click(hamburgerElement);
-  expect(screen.getByText(/New Stock/)).toBeInTheDocument(); // Menu is opened
+  test("displays the current time", () => {
+    const timeElement = screen.getByText(/^\d{1,2}:\d{2}:\d{2}$/); // Matches the format of the displayed time
+    expect(timeElement).toBeInTheDocument();
+  });
+
+  test("shows the menu when the hamburger is clicked", () => {
+    const hamburgerElement = screen.getByLabelText(/Show menu/);
+    expect(screen.queryByText(/New Stock/)).not.toBeInTheDocument(); // Menu is initially closed
+    fireEvent.click(hamburgerElement);
+    expect(screen.getByText(/New Stock/)).toBeInTheDocument(); // Menu is opened
+  });
 });
