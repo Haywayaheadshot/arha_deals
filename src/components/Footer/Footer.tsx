733a1eba79b150082f9b29f9335f0fb02d12f9a4
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-04-26 01:06:01 +0000

    Make sure ul has li item as direct child

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -42,36 +42,39 @@ const Footer = () => {
       <section className="bg-black text-primary w-full py-5 flex flex-col justify-center items-center gap-10">
         <h3>Follow Us On Social Media</h3>
         <ul className="flex flex-row gap-10 py-3">
-          <a
-            href="https://www.tiktok.com/@arhadeals"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <li className="flex flex-col items-center gap-2">
+          <li>
+            <a
+              href="https://www.tiktok.com/@arhadeals"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-col items-center gap-2"
+            >
               <FaTiktok />
               <span>Tik Tok</span>
-            </li>
-          </a>
-          <a
-            href="https://www.instagram.com/arha_deals/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <li className="flex flex-col items-center gap-2">
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/arha_deals/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-col items-center gap-2"
+            >
               <FaInstagram />
               <span>Instagram</span>
-            </li>
-          </a>
-          <a
-            href="https://twitter.com/arha_deals"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <li className="flex flex-col items-center gap-2">
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/arha_deals"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-col items-center gap-2"
+            >
               <FaTwitter />
               <span>Twitter</span>
-            </li>
-          </a>
+            </a>
+          </li>
         </ul>
         <span className="flex flex-row justify-center items-center gap-2 py-5">
           <FaRegCopyright />
