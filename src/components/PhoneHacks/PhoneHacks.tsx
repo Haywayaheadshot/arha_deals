d0e9bf59d9b994516602087d4274d6ed5467edcd
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-04-28 02:20:52 +0000

    Receive and display phone hacks on UI

--- src/components/PhoneHacks/PhoneHacks.tsx
+++ src/components/PhoneHacks/PhoneHacks.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
+import RootState from "../../redux/phoneHacks/PhoneHacks";
 
 const PhoneHacks = () => {
+  const hacks = useSelector((state: typeof RootState) => state.hacks.hacks);
+
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelectChange = (selectedOption) => {
+    setSelectedOption(selectedOption.value);
+  };
+
+  const filteredHacks = hacks.filter((hack) => {
+    if (selectedOption === "") {
+      return true;
+    } else {
+      return hack.os === selectedOption;
+    }
+  });
+
   const options = [
-    { value: null, label: "All" },
+    { value: "", label: "All" },
     { value: "android", label: "Android" },
     { value: "iphone", label: "iPhone" },
   ];
 
   const animatedComponents = makeAnimated();
 
-  // const [validSelect, setValidSelect] = useState(false);
+  const capitalize = (str) => {
+    return str.toUpperCase();
+  };
 
   return (
     <div className="py-10 px-4 flex flex-col gap-4">
@@ -28,74 +48,34 @@ const PhoneHacks = () => {
           options={options}
           components={animatedComponents}
           defaultValue={[options[0]]}
+          onChange={handleSelectChange}
         />
       </section>
       <section className="flex flex-col gap-6">
-        <div className="border-2 rounded-lg flex flex-col px-5 py-3 justify-center items-center">
-          <ul className="flex flex-row justify-between gap-10">
-            <li>
-              <h1>Hack Title</h1>
-            </li>
-            <li>
-              <h2>For iphone / android</h2>
-            </li>
-          </ul>
-          <p>Hack Description</p>
-          <section>Video on how to perform hack</section>
-          <section>Pros of the hack</section>
-        </div>
-        <div className="border-2 rounded-lg flex flex-col px-5 py-3 justify-center items-center">
-          <ul className="flex flex-row justify-between gap-10">
-            <li>
-              <h1>Hack Title</h1>
-            </li>
-            <li>
-              <h2>For iphone / android</h2>
-            </li>
-          </ul>
-          <p>Hack Description</p>
-          <section>Video on how to perform hack</section>
-          <section>Pros of the hack</section>
-        </div>
-        <div className="border-2 rounded-lg flex flex-col px-5 py-3 justify-center items-center">
-          <ul className="flex flex-row justify-between gap-10">
-            <li>
-              <h1>Hack Title</h1>
-            </li>
-            <li>
-              <h2>For iphone / android</h2>
-            </li>
-          </ul>
-          <p>Hack Description</p>
-          <section>Video on how to perform hack</section>
-          <section>Pros of the hack</section>
-        </div>
-        <div className="border-2 rounded-lg flex flex-col px-5 py-3 justify-center items-center">
-          <ul className="flex flex-row justify-between gap-10">
-            <li>
-              <h1>Hack Title</h1>
-            </li>
-            <li>
-              <h2>For iphone / android</h2>
-            </li>
-          </ul>
-          <p>Hack Description</p>
-          <section>Video on how to perform hack</section>
-          <section>Pros of the hack</section>
-        </div>
-        <div className="border-2 rounded-lg flex flex-col px-5 py-3 justify-center items-center">
-          <ul className="flex flex-row justify-between gap-10">
-            <li>
-              <h1>Hack Title</h1>
-            </li>
-            <li>
-              <h2>For iphone / android</h2>
-            </li>
-          </ul>
-          <p>Hack Description</p>
-          <section>Video on how to perform hack</section>
-          <section>Pros of the hack</section>
-        </div>
+        {filteredHacks.map((hack: typeof hacks) => (
+          <div
+            key={hack.id}
+            className="border-2 rounded-lg flex flex-col px-5 py-3 justify-center items-center"
+          >
+            <ul className="flex flex-row justify-between gap-10">
+              <li>
+                <h1>{hack.title}</h1>
+              </li>
+              <li>
+                <h2>{capitalize(hack.os)}</h2>
+              </li>
+            </ul>
+            <p>{hack.description}</p>
+            <iframe
+              width="300"
+              height="300"
+              src="https://www.youtube.com/embed/VIDEO_ID_HERE"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+            <section>{hack.advantages}</section>
+          </div>
+        ))}
       </section>
     </div>
   );
--- src/redux/configureStore.tsx
+++ src/redux/configureStore.tsx
@@ -1,12 +1,14 @@
 import { createLogger } from "redux-logger";
 import { configureStore } from "@reduxjs/toolkit";
 import newsSlice from "./news/News";
+import phoneHacksSlice from "./phoneHacks/PhoneHacks";
 
 const logger = createLogger();
 
 const store = configureStore({
   reducer: {
     news: newsSlice,
+    hacks: phoneHacksSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
