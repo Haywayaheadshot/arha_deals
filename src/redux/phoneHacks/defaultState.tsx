91168cb74175c3a49bbaab339a447b4ab4fa0004
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-02 00:56:04 +0000

    Change React.fragment to <></>

--- src/redux/phoneHacks/defaultState.tsx
+++ src/redux/phoneHacks/defaultState.tsx
@@ -7,7 +7,7 @@ const defaultState = [
     title: "Guided Access",
     os: "iphone",
     description: (
-      <React.Fragment>
+      <>
         <li>1. Open the Settings app on your iPhone.</li>
         <li>2. Tap &quot;General&quot; and then&quot;Accessibility.&quot;</li>
         <li>3. Scroll down to &quot;Guided Access&quot; and toggle it on.</li>
@@ -31,11 +31,11 @@ const defaultState = [
           Access to lock your iPhone to a single app and limit access to certain
           parts of the screen.
         </li>
-      </React.Fragment>
+      </>
     ),
     video_url: "https://www.youtube.com/embed/OEaOLPhaW5E",
     advantages: (
-      <React.Fragment>
+      <>
         Guided Access is a useful feature on iPhones that allows you to lock
         your device to a single app and restrict access to certain parts of the
         screen.
@@ -44,7 +44,7 @@ const defaultState = [
         children&apos;s access to certain apps or for people who need to focus
         on a specific task. Here are the steps to perform Guided Access on
         iPhones.
-      </React.Fragment>
+      </>
     ),
   },
   {
@@ -52,7 +52,7 @@ const defaultState = [
     title: "Encrypt Your Device",
     os: "android",
     description: (
-      <React.Fragment>
+      <>
         <li>1. Open the Settings app on your phone.</li>
         <li>2. Tap &quot;Security & Location&quot; or &quot;Security.&quot;</li>
         <li>
@@ -64,17 +64,17 @@ const defaultState = [
           process may take some time, and your phone may restart several times
           during the encryption process.
         </li>
-      </React.Fragment>
+      </>
     ),
     video_url: "https://www.youtube.com/embed/AYcqo5CEKgI",
     advantages: (
-      <React.Fragment>
+      <>
         Encrypting your Android phone ensures that your personal data remains
         safe even if your phone falls into the wrong hands. Encryption scrambles
         your data so that it&apos;s unreadable without the correct decryption
         key, which is tied to your lock screen password or PIN. Here&apos;s how
         to encrypt your Android phone:
-      </React.Fragment>
+      </>
     ),
   },
   {
@@ -82,7 +82,7 @@ const defaultState = [
     title: "Use Shortcuts",
     os: "iphone",
     description: (
-      <React.Fragment>
+      <>
         <li>1. Download the Shortcuts app from the App Store.</li>
         <li>
           2. Open the app and tap the &quot;+&quot; icon to create a new
@@ -96,16 +96,16 @@ const defaultState = [
         <li>
           5. To trigger the shortcut, say &quot;Hey Siri, [shortcut name].&quot;
         </li>
-      </React.Fragment>
+      </>
     ),
     video_url: "https://www.youtube.com/embed/K4gLTEHumHI",
     advantages: (
-      <React.Fragment>
+      <>
         Shortcuts is an app that allows you to create custom automations on your
         iPhone. With Shortcuts, you can create a series of actions that are
         triggered by a single command, such as &quot;Hey Siri.&quot; Here&apos;s
         how touse Shortcuts:
-      </React.Fragment>
+      </>
     ),
   },
   {
@@ -113,7 +113,7 @@ const defaultState = [
     title: "Jailbreak Your iPhone",
     os: "iphone",
     description: (
-      <React.Fragment>
+      <>
         <li>
           1. Note: Jailbreaking can potentially void your warranty and may
           expose your device to security risks. Proceed at your own risk.
@@ -131,17 +131,17 @@ const defaultState = [
           5. Once your iPhone is jailbroken, you can install third-partyapps and
           tweaks from sources other than the App Store.
         </li>
-      </React.Fragment>
+      </>
     ),
     video_url: "https://www.youtube.com/embed/K4gLTEHumHI",
     advantages: (
-      <React.Fragment>
+      <>
         Jailbreaking is the process of removing the limitations imposed by Apple
         on your iPhone. Jailbreaking allows you to install apps and tweaks that
         are not available on the App Store, customize your iPhone&apos;s
         appearance and functionality, and access system files and settings.
         Here&apos;s how to jailbreak your iPhone:
-      </React.Fragment>
+      </>
     ),
   },
   {
@@ -149,7 +149,7 @@ const defaultState = [
     title: "Enable Developer Options",
     os: "android",
     description: (
-      <React.Fragment>
+      <>
         <li>1. Open the Settings app on your phone.</li>
         <li>
           2. Scroll down and tap &quot;About phone&quot; or &quot;About
@@ -164,15 +164,15 @@ const defaultState = [
           5. You should see a message that says &quot;You are now a
           developer!&quot;.
         </li>
-      </React.Fragment>
+      </>
     ),
     video_url: "https://www.youtube.com/embed/AYcqo5CEKgI",
     advantages: (
-      <React.Fragment>
+      <>
         Developer options are hidden settings that allow you to customize and
         optimize your Android phone. Here&apos;s how to enable Developer Options
         on your Android phone:
-      </React.Fragment>
+      </>
     ),
   },
   {
@@ -180,7 +180,7 @@ const defaultState = [
     title: "Use USB Debugging",
     os: "android",
     description: (
-      <React.Fragment>
+      <>
         <li>1. Enable Developer Options (check list for hack).</li>
         <li>2. Open the Settings app on your phone.</li>
         <li>3. Tap &quot;System&quot; or &quot;System & updates.&quot;</li>
@@ -192,15 +192,15 @@ const defaultState = [
           Follow the on-screen instructions to allow USB debugging on your
           phone.
         </li>
-      </React.Fragment>
+      </>
     ),
     video_url: "https://www.youtube.com/embed/AYcqo5CEKgI",
     advantages: (
-      <React.Fragment>
+      <>
         USB debugging is a feature that allows you to connect your Android phone
         to a computer and access advanced settings and functions. Here&apos;s
         how to enable USB debugging on your Android phone:
-      </React.Fragment>
+      </>
     ),
   },
 ];
