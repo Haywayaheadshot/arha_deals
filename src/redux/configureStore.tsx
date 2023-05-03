6cf8a6b05fe03910a5465636d2769cbbd58a139d
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-03 00:59:22 +0000

    Add exclusive items to store

--- src/redux/configureStore.tsx
+++ src/redux/configureStore.tsx
@@ -2,6 +2,7 @@ import { createLogger } from "redux-logger";
 import { configureStore } from "@reduxjs/toolkit";
 import newsSlice from "./news/News";
 import phoneHacksReducer from "./phoneHacks/PhoneHacks";
+import exclusiveItemsReducer from "./exclusiveItems/ExclusiveItems";
 
 const logger = createLogger();
 
@@ -9,6 +10,7 @@ const store = configureStore({
   reducer: {
     news: newsSlice,
     hacks: phoneHacksReducer,
+    items: exclusiveItemsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
