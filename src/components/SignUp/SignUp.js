93302755c6af9833fd57333eb6333702aaac90e0
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-22 16:31:35 +0000

    Add signup page

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <form>
+      <label htmlFor="name">
+        <input htmlFor="name" />
+      </label>
+    </form>
+  );
+}
+
+export default SignUp;
