39ac7377c00a12ff31b58636e6baad675ea0a884
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-26 02:11:57 +0000

    put logo in separate component

---
+++ src/components/Shared/Logo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaCrown } from "react-icons/fa";
+import ChangeThemes from "./ChangeThemes";
+
+const Logo = () => {
+  return (
+    <NavLink to="/" className="flex flex-col justify-center items-center">
+      <h1 className="text-xl text-secondary flex flex-wrap py-3">
+        Tailored
+        <FaCrown />
+        Budget
+      </h1>
+      <section className="hidden desktop:flex">
+        <ChangeThemes />
+      </section>
+    </NavLink>
+  );
+};
+
+export default Logo;
