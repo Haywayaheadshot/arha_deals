d713140523f17317142ee89b9f0d9f2a33b34ad0
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-26 02:10:06 +0000

    add daisyui to props

--- tailwind.config.js
+++ tailwind.config.js
@@ -83,4 +83,13 @@ export default {
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
 };
