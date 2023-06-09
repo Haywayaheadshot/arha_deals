aa1be6b70780ce1a47b15a66a64012525b1ef4fc
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-06-09 01:45:09 +0000

    receive cart from redux store

--- src/components/ConfirmOrder/ConfirmOrder.tsx
+++ src/components/ConfirmOrder/ConfirmOrder.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { getCart } from "../../redux/cart/Cart";
+import { RootState } from "../../redux/configureStore";
 
 const ConfirmOrder = () => {
+  const cart = useSelector((state: RootState) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCart() as any);
+  }, [dispatch]);
+
+  console.log(cart);
+
   return (
-    <div>
+    <div className="pt-20">
       <h1>
         If the cart_items table has items it will be mapped here. Else this page
         should not display.
